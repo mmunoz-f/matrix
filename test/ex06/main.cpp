38d7594cac5aac93b217372b7fd54ad96958c3be
mmunoz-f <miguelmunozfdez@gmail.com>
2024-10-03 18:47:05 +0200

    ex06 checked

--- test/ex06/main.cpp
+++ test/ex06/main.cpp
@@ -4,61 +4,46 @@
 
 #include "../../matrix.h"
 
-#define TYPE int 
-
-#define VECTOR matrix::Vector<TYPE>
-
 TEST(CrossProduct, Unit)
 {
-    VECTOR vector1{0, 0, 1};
-    VECTOR vector2{1, 0, 0};
+    matrix::Vector<int, 3> vector1 = {0, 0, 1};
+    matrix::Vector<int, 3> vector2 = {1, 0, 0};
 
-    VECTOR result = matrix::cross_product(vector1, vector2);
+    matrix::Vector<int, 3> result = matrix::cross_product(vector1, vector2);
 
-    EXPECT_EQ(vector1, VECTOR({0, 0, 1}));
-    EXPECT_EQ(vector2, VECTOR({1, 0, 0}));
-
-    EXPECT_EQ(result, VECTOR({0, 1, 0}));
+    matrix::Vector<int, 3> expt = {0, 1, 0};
+    EXPECT_EQ(result, expt);
 }
 
 TEST(CrossProduct, Simple)
 {
-    VECTOR vector1{1, 2, 3};
-    VECTOR vector2{4, 5, 6};
+    matrix::Vector<int, 3> vector1{1, 2, 3};
+    matrix::Vector<int, 3> vector2{4, 5, 6};
 
-    VECTOR result = matrix::cross_product(vector1, vector2);
+    matrix::Vector<int, 3> result = matrix::cross_product(vector1, vector2);
 
-    EXPECT_EQ(vector1, VECTOR({1, 2, 3}));
-    EXPECT_EQ(vector2, VECTOR({4, 5, 6}));
-
-    EXPECT_EQ(result, VECTOR({-3, 6, -3}));
+    matrix::Vector<int, 3> expt = {-3, 6, -3};
+    EXPECT_EQ(result, expt);
 }
 
 TEST(CrossProduct, Simple1)
 {
-    VECTOR vector1{4, 2, -3};
-    VECTOR vector2{-2, -5, 16};
+    matrix::Vector<int, 3> vector1{4, 2, -3};
+    matrix::Vector<int, 3> vector2{-2, -5, 16};
 
-    VECTOR result = matrix::cross_product(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({4, 2, -3}));
-    EXPECT_EQ(vector2, VECTOR({-2, -5, 16}));
-
-    EXPECT_EQ(result, VECTOR({17, -58, -16}));
+    matrix::Vector<int, 3> result = matrix::cross_product(vector1, vector2);
+    
+    matrix::Vector<int, 3> expt = {17, -58, -16};
+    EXPECT_EQ(result, expt);
 }
 
-TEST(CrossProduct, Not3Dim)
-{
-    VECTOR vector1{0, 1};
-    VECTOR vector2{1, 0};
+// TEST(CrossProduct, NotComp) // This does not compile
+// {
+//     matrix::Vector<int, 2> vector1{0, 1};
+//     matrix::Vector<int, 2> vector2{1, 0};
 
-    EXPECT_THROW({
-        VECTOR result = matrix::cross_product(vector1, vector2);
-    }, std::runtime_error);
-
-    EXPECT_EQ(vector1, VECTOR({0, 1}));
-    EXPECT_EQ(vector2, VECTOR({1, 0}));
-}
+//     matrix::Vector<int, 2> result = matrix::cross_product(vector1, vector2);
+// }
 
 /***************************** */
 
