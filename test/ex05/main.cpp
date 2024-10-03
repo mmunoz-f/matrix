ba45a9778bccb3604e0d998bb2b258d58f78ceee
mmunoz-f <miguelmunozfdez@gmail.com>
2024-10-03 18:41:20 +0200

    ex05 checked

--- test/ex05/main.cpp
+++ test/ex05/main.cpp
@@ -4,71 +4,52 @@
 
 #include "../../matrix.h"
 
-#define TYPE float 
-
-#define VECTOR matrix::Vector<TYPE>
-
 TEST(CosAngle, degree_00)
 {
-    VECTOR vector1{1.f, 0.f};
-    VECTOR vector2{1.f, 0.f};
+    matrix::Vector<float, 2> vector1 = {1.f, 0.f};
+    matrix::Vector<float, 2> vector2 = {1.f, 0.f};
 
     float result = matrix::angle_cos(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({1.f, 0.f}));
-    EXPECT_EQ(vector2, VECTOR({1.f, 0.f}));
 
     EXPECT_FLOAT_EQ(result, 1.f);
 }
 
 TEST(CosAngle, degree_90)
 {
-    VECTOR vector1{1.f, 0.f};
-    VECTOR vector2{0.f, 1.f};
+    matrix::Vector<float, 2> vector1 = {1.f, 0.f};
+    matrix::Vector<float, 2> vector2 = {0.f, 1.f};
 
     float result = matrix::angle_cos(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({1.f, 0.f}));
-    EXPECT_EQ(vector2, VECTOR({0.f, 1.f}));
 
     EXPECT_FLOAT_EQ(result, 0.f);
 }
 
 TEST(CosAngle, degree_180)
 {
-    VECTOR vector1{1.f, 0.f};
-    VECTOR vector2{-1.f, 0.f};
+    matrix::Vector<float, 2> vector1 = {1.f, 0.f};
+    matrix::Vector<float, 2> vector2 = {-1.f, 0.f};
 
     float result = matrix::angle_cos(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({1.f, 0.f}));
-    EXPECT_EQ(vector2, VECTOR({-1.f, 0.f}));
 
     EXPECT_FLOAT_EQ(result, -1.f);
 }
 
 TEST(CosAngle, degree_270)
 {
-    VECTOR vector1{1.f, 0.f};
-    VECTOR vector2{0.f, -1.f};
+    matrix::Vector<float, 2> vector1 = {1.f, 0.f};
+    matrix::Vector<float, 2> vector2 = {0.f, -1.f};
 
     float result = matrix::angle_cos(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({1.f, 0.f}));
-    EXPECT_EQ(vector2, VECTOR({0.f, -1.f}));
 
     EXPECT_FLOAT_EQ(result, 0.f);
 }
 
 TEST(CosAngle, Simple)
 {
-    VECTOR vector1{1.f, 2.f, 3.f};
-    VECTOR vector2{4.f, 5.f, 6.f};
+    matrix::Vector<float, 3> vector1 = {1.f, 2.f, 3.f};
+    matrix::Vector<float, 3> vector2 = {4.f, 5.f, 6.f};
 
     float result = matrix::angle_cos(vector1, vector2);
-
-    EXPECT_EQ(vector1, VECTOR({1.f, 2.f, 3.f}));
-    EXPECT_EQ(vector2, VECTOR({4.f, 5.f, 6.f}));
 
     EXPECT_FLOAT_EQ(result,  0.974631846);
 }
