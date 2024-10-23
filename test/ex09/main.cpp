51a9c4df1241d179d7b79eee71ef09eb8a80e726
mmunoz-f <miguelmunozfdez@gmail.com>
2024-10-23 21:00:10 +0200

    ex09 rechecked

--- test/ex09/main.cpp
+++ test/ex09/main.cpp
@@ -4,41 +4,43 @@
 
 #include "../../matrix.h"
 
-#define TYPE int
-
-#define MATRIX matrix::Matrix<TYPE>
-
 TEST(MatrixTraspose, Identity)
 {
-    MATRIX matrix{{1, 0}, {0, 1}};
+    matrix::Matrix<int, 2, 2> matrix = {1, 0,
+                                        0, 1};
 
-    MATRIX result = matrix.traspose();
+    matrix::Matrix<int, 2, 2> result = matrix.traspose();
 
-    EXPECT_EQ(matrix, MATRIX({{1, 0}, {0, 1}}));
-
-    EXPECT_EQ(result, MATRIX({{1, 0}, {0, 1}}));
+    matrix::Matrix<int, 2, 2> expect = {1, 0,
+                                        0, 1};
+    EXPECT_EQ(result, expect);
 }
 
 TEST(MatrixTraspose, Simple)
 {
-    MATRIX matrix{{2, -5, 0}, {4, 3, 7}, {-2, 3, 4}};
+    matrix::Matrix<int, 3, 3> matrix = { 2, -5,  0,
+                                         4,  3,  7,
+                                        -2,  3,  4};
 
-    MATRIX result = matrix.traspose();
+    matrix::Matrix<int, 3, 3> result = matrix.traspose();
 
-    EXPECT_EQ(matrix, MATRIX({{2, -5, 0}, {4, 3, 7}, {-2, 3, 4}}));
-
-    EXPECT_EQ(result, MATRIX({{2, 4, -2}, {-5, 3, 3}, {0, 7, 4}}));
+    matrix::Matrix<int, 3, 3> expect = { 2,  4, -2,
+                                        -5,  3,  3,
+                                         0,  7,  4};
+    EXPECT_EQ(result, expect);
 }
 
 TEST(MatrixTraspose, NotSquare)
 {
-    MATRIX matrix{{1, 0, 1}, {0, 1, 1}};
+    matrix::Matrix<int, 2, 3> matrix = {1, 0, 1,
+                                        0, 1, 1};
 
-    MATRIX result = matrix.traspose();
+    matrix::Matrix<int, 3, 2> result = matrix.traspose();
 
-    EXPECT_EQ(matrix, MATRIX({{1, 0, 1}, {0, 1, 1}}));
-
-    EXPECT_EQ(result, MATRIX({{1, 0}, {0, 1}, {1, 1}}));
+    matrix::Matrix<int, 3, 2> expect = {1, 0,
+                                        0, 1,
+                                        1, 1};
+    EXPECT_EQ(result, expect);
 }
 
 /***************************** */
